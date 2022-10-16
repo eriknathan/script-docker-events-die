3dba4e538a77bb69ce9d0db0f99b146e082fe47c
Erik Nathan <erik@eriknathan.localdomain>
2022-10-16 10:55:30 -0300

    Subindo o script

---
+++ events.py
@@ -0,0 +1,19 @@
+import docker
+import datetime
+import requests
+import os
+
+client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+webhook_url = "#"
+
+for event in client.events(decode=True, filters={"event": "die"}):
+    container_id = event["id"]
+    container_name = event["Actor"]["Attributes"]["name"]
+    epoch_date = event["time"] 
+    date_time = datetime.datetime.fromtimestamp(epoch_date)  
+
+    response = {"content": f'O CONTAINER FOI DERRUBADO!!! {os.linesep}- ID do Container: {container_id} {os.linesep}- Nome do Container: {container_name} {os.linesep}- Data e Hora: {date_time}'}
+
+    print(response)
+    requests.post(webhook_url, data=response)
+    
